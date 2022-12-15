d9233827858918d798edd4e964339d28566b794b
itskk <itskk@itskk.itskk.live>
2022-12-16 00:00:21 +0530

    9

--- Two_Sum.java
+++ Two_Sum.java
@@ -1,4 +1,4 @@
-public class Two_Sum {
+public class 1_Two_Sum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> map=new HashMap();
         for(int i=0;i<nums.length;i++){
